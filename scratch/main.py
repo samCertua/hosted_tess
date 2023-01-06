ba46a3b20a86ed6ce2c7da5c6b4683bf9ae0ff02
samCertua <51408936+samCertua@users.noreply.github.com>
2023-01-06 13:27:41 +0000

    vectors

--- scratch/main.py
+++ scratch/main.py
@@ -1,8 +1,8 @@
-from gpt_index import GPTTreeIndex, SimpleDirectoryReader
+from gpt_index import GPTTreeIndex, SimpleDirectoryReader, GPTSimpleVectorIndex
 import os
 os.environ['OPENAI_API_KEY'] = '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'
 
-def main():
+def tree():
     # documents = SimpleDirectoryReader('scratch/data').load_data()
     # index = GPTTreeIndex(documents)
     # index.save_to_disk('policy_docs.json')
@@ -10,5 +10,13 @@ def main():
     print(new_index.query("What are the cancellation terms?"))
     pass
 
+def vector():
+    documents = SimpleDirectoryReader('scratch/data').load_data()
+    index = GPTSimpleVectorIndex(documents)
+    index.save_to_disk('policy_docs_vector.json')
+    new_index = GPTSimpleVectorIndex.load_from_disk('policy_docs_vector.json')
+    print(new_index.query("What are the cancellation terms?"))
+    pass
+
 if __name__ == '__main__':
-    main()
+    vector()
