9087c5bdbffec9189884ffb2ab5eebca054c2e12
samCertua <51408936+samCertua@users.noreply.github.com>
2023-01-17 16:32:59 +0000

    updated api key and prompt

--- t_and_c_main.py
+++ t_and_c_main.py
@@ -55,6 +55,6 @@ if st.session_state['generated']:
 
     for i in range(len(st.session_state['generated'])):
         with chat:
-            message(st.session_state["generated"][i], key=str(i))
             message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
 
