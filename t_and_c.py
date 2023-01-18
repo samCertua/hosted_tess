e47377b9ef8f4d2fe7aa816d47c47d6416ec83ff
samCertua <51408936+samCertua@users.noreply.github.com>
2023-01-18 11:40:33 +0000

    debugging

--- t_and_c.py
+++ t_and_c.py
@@ -127,6 +127,7 @@ def populate_pinecone():
 def distributor_matches(index, query, distributors, number_of_results):
     results = []
     for d in distributors:
+        print(d)
         matches = index.query(
             vector=query,
             top_k=1,
